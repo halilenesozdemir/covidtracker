4363415a61005e87b8d4d4875f3c8ced4d561703
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-27 19:39:56 +0300

    Covid-19-Tracker with React-Mui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='app'>
+        <h1>Let's build a COVİD 19 tracker... Yeeey</h1>
+        {/* Header */}
+        {/* Title + Select input dropdown field */}
+
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+
+        {/* Table */}
+        {/* Graph */}
+
+        {/* Map */}
+      </div>
+    </>
   );
 }
 
